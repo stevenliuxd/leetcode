55873ea6e8c742f66893ebae73aa8e7ef90a3e6b
Steven <shnauzerr@hotmail.com>
2022-08-28 23:29:08 -0600

    updated class

--- Fundamentals/class_objects.py
+++ Fundamentals/class_objects.py
@@ -8,9 +8,26 @@ class Robot:
         print("My name is " + self.name)
         print("My color is " + self.color)
 
+class Person:
+    def __init__(self, name, personality, isSitting):
+        self.name = name
+        self.personality = personality
+        self.isSitting = isSitting
+
+    def sitDown(self):
+        self.isSitting = True
+    
+    def standUp(self):
+        self.isSitting = False
+
 r1 = Robot("Tom", "red", 30)
 r2 = Robot("Jerry", "blue", 40)
-
 r1.introduce_self()
 r2.introduce_self()
 
+p1 = Person("Alice", "aggresive", False)
+p2 = Person("Becky", "talkative", True)
+p1.robot_owned = r2
+p2.robot_owned = r1
+
+p1.robot_owned.introduce_self()
